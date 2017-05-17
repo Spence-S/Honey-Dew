390adf966445b022058713455964d93a6fbf5f88
Spencer Snyder <sasnyde2@gmail.com>
2017-05-17 16:02:33 -0400

    router pages added

---
+++ src/containers/LandingPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Nav, Navbar, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
+
+class LandingPage extends Component{
+  render(){
+    return(
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
---
+++ src/containers/Main.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Link
+} from 'react-router-dom'
+import { Nav, Navbar, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap'
+
+import LandingPage from './LandingPage';
+import Login from './Login';
+import App from './App';
+
+class Main extends Component{
+  render(){
+    return(
+      <Router>
+        <div>
+          <Navbar collapseOnSelect>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <Link to="/">HoneyDew</Link>
+              </Navbar.Brand>
+            <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+            <Nav pullRight>
+              <LinkContainer to="/"><NavItem eventKey={1}>Home</NavItem></LinkContainer>
+              <LinkContainer to="/App"><NavItem eventKey={2}>App</NavItem></LinkContainer>
+              <LinkContainer to="/Login"><NavItem eventKey={3}>Login</NavItem></LinkContainer>
+              <NavDropdown eventKey={4} title="Dropdown" id="basic-nav-dropdown">
+                <MenuItem eventKey={4.1}>Action</MenuItem>
+                <MenuItem eventKey={4.2}>Another action</MenuItem>
+                <MenuItem eventKey={4.3}>Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={4.4}>Separated link</MenuItem>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+          </Navbar>
+          <Route exact path="/" component={LandingPage}/>
+
+           <Route exact path="/App" render={() =>(
+            !this.props.authState.isLoggedIn ?
+                <Redirect to='/Login' />  : <App />
+            )}
+           />
+          <Route path="/Login" component={Login}/>
+        </div>
+      </Router>
+    )
+  }
+}
+
+function mapStateToProps(state){
+  return { ...state }
+}
+
+export default connect(mapStateToProps, null)(Main);
