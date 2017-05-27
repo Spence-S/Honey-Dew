daeb9971141bdbfea7f383ba3c28dd00d790c503
Spencer Snyder <sasnyde2@gmail.com>
2017-05-27 14:25:33 -0400

    account page styles

--- src/containers/Account.js
+++ src/containers/Account.js
@@ -5,30 +5,47 @@ const Account = (props) => (
     <div className='col-md-6'>
       <h3> {props.authState.facebook.name}</h3>
       <ul className="list-group">
-        <li className='list-group-item clearfix'>
-            First Name: {props.authState.facebook.name.split(" ")[0]}
-            <button className="btn btn-link pull-right">Change</button>
-        </li>
-        <li className='list-group-item clearfix'>
-          Last Name: {props.authState.facebook.name.split(" ")[1]}
-            <button className="btn btn-link pull-right">Change</button>
-        </li>
-        <li className='list-group-item clearfix'>
-          Primary Email: {props.authState.facebook.email}
-          <button className="btn btn-link pull-right">Change</button>
-        </li>
-        <li className='list-group-item clearfix'>
-          Phone:
-          <button className="btn btn-link pull-right">Change</button>
-        </li>
-        <li className='list-group-item clearfix'>
-          Facebook Linked: true
-          <button className="btn btn-link pull-right">Change</button>
-        </li>
-        <li className='list-group-item clearfix'>
-          : true
-          <button className="btn btn-link pull-right">Change</button>
-        </li>
+        <div>
+          <li className='list-group-item'>
+            <span>First Name: {props.authState.facebook.name.split(" ")[0]}</span>
+            <button className="btn btn-link">Change</button>
+          </li>
+        </div>
+        <div>
+          <li className='list-group-item'>
+            Last Name: {props.authState.facebook.name.split(" ")[1]}
+            <button className="btn btn-link">Change</button>
+          </li>
+        </div>
+        <div>
+          <li className='list-group-item'>
+            Primary Email: {props.authState.facebook.email}
+            <button className="btn btn-link">Change</button>
+          </li>
+        </div>
+        <div>
+          <li className='list-group-item'>
+            <button className="btn btn-link"> Change Picture</button>
+          </li>
+        </div>
+        <div>
+          <li className='list-group-item'>
+            Phone:
+            <button className="btn btn-link">Change</button>
+          </li>
+        </div>
+        <div>
+          <li className='list-group-item'>
+            <p>Facebook Linked: true</p>
+            <button className="btn btn-link">Change</button>
+          </li>
+        </div>
+        <div>
+          <li className='list-group-item'>
+            something here: true
+            <button className="btn btn-link">Change</button>
+          </li>
+        </div>
       </ul>
     </div>
   </div>
