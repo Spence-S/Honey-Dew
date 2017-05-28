b1094b09633553943e3a76f007995671e9861c60
Spencer Snyder <sasnyde2@gmail.com>
2017-05-28 16:36:36 -0400

    created users actions and reducers

--- src/containers/Account.js
+++ src/containers/Account.js
@@ -1,54 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Account = (props) => (
-  <div className='row'>
-    <div className='col-md-6'>
-      <h3> {props.authState.facebook.name}</h3>
-      <ul className="list-group">
-        <div>
-          <li className='list-group-item'>
-            <span>First Name: {props.authState.facebook.name ? props.authState.facebook.name.split(" ")[0] : ''}</span>
-            <button className="btn btn-link">Change</button>
-          </li>
-        </div>
-        <div>
-          <li className='list-group-item'>
-            Last Name: {props.authState.facebook.name ? props.authState.facebook.name.split(" ")[1] : ''}
-            <button className="btn btn-link">Change</button>
-          </li>
-        </div>
-        <div>
-          <li className='list-group-item'>
-            Primary Email: {props.authState.facebook.email}
-            <button className="btn btn-link">Change</button>
-          </li>
-        </div>
-        <div>
-          <li className='list-group-item'>
-            <button className="btn btn-link"> Change Picture</button>
-          </li>
-        </div>
-        <div>
-          <li className='list-group-item'>
-            Phone:
-            <button className="btn btn-link">Change</button>
-          </li>
-        </div>
-        <div>
-          <li className='list-group-item'>
-            <p>Facebook Linked: true</p>
-            <button className="btn btn-link">Change</button>
-          </li>
-        </div>
-        <div>
-          <li className='list-group-item'>
-            something here: true
-            <button className="btn btn-link">Change</button>
-          </li>
-        </div>
-      </ul>
-    </div>
-  </div>
-)
+export default class Account extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      harryBalls: true
+    }
+  }
 
-export default Account;
+  render(){
+    return(
+      <div className='row'>
+        <div className='col-md-6'>
+          <h3> </h3>
+          <ul className="list-group">
+            <div>
+              <li className='list-group-item'>
+                <span>First Name: </span>
+                <button className="btn btn-link">Change</button>
+              </li>
+            </div>
+            <div>
+              <li className='list-group-item'>
+                Last Name:
+                <button className="btn btn-link">Change</button>
+              </li>
+            </div>
+            <div>
+              <li className='list-group-item'>
+                Primary Email:
+                <button className="btn btn-link">Change</button>
+              </li>
+            </div>
+            <div>
+              <li className='list-group-item'>
+                <button className="btn btn-link"> Change Picture</button>
+              </li>
+            </div>
+            <div>
+              <li className='list-group-item'>
+                Phone:
+                <button className="btn btn-link">Change</button>
+              </li>
+            </div>
+            <div>
+              <li className='list-group-item'>
+                <p>Facebook Linked: true</p>
+                <button className="btn btn-link">Change</button>
+              </li>
+            </div>
+            <div>
+              <li className='list-group-item'>
+                something here: true
+                <button className="btn btn-link">Change</button>
+              </li>
+            </div>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
